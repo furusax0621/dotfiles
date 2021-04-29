ccb97d5a7a1e8f3d1b080f77ea268dfe0ae1b0c5
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2021-04-29 21:25:48 +0900

    remove vscode setting files

--- editor.sh
+++ editor.sh
@@ -2,14 +2,6 @@
 
 dir=$(cd "$(dirname "${0}")" && pwd)
 
-# VSCodeがインストール済であれば環境設定を同期する
-if type code >/dev/null 2>&1; then
-    # 拡張機能をインストール
-    "${dir}/vscode/install-vscode-extensions.sh"
-    # settings.jsonを同期
-    ln -sf "${dir}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
-fi
-
 # Vimの環境設定を同期する
 VIMPATH=${HOME}/.vim
 if [ ! -d "${VIMPATH}" ]; then
@@ -17,4 +9,3 @@ if [ ! -d "${VIMPATH}" ]; then
 fi
 ln -sf "${dir}/vim/*.vim" "${VIMPATH}/"
 ln -sf "${dir}/vim/.vimrc" "${HOME}/.vimrc"
-
